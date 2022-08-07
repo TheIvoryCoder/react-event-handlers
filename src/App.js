e5ae8e4ec1b56f9ade61b06608666ba9fcfb9694
Charlie Cureton <Ccureton21@gmail.com>
2022-08-07 14:47:29 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
+import logo from './Logo.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" />
+        <h1 className="App-title">TheIvoryCoder</h1>
+        <h2 className="App-subtitle">Event Handlers in React.js</h2>
+
+        <Button variant="contained" style={{
+          backgroundColor: 'red',
+          fontSize: 25,
+          paddingLeft: 25,
+          paddingRight: 25
+        }}>Subscribe</Button>
+
+        <a className="App-link" href="https://github.com/theivorycoder/react-event-handlers">https://github.com/theivorycoder/react-event-handlers</a>
       </header>
     </div>
   );
