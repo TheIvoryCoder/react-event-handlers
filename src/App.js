3e28c05c8082625ca140df7b8d350e6fa99e4ae6
Charlie Cureton <Ccureton21@gmail.com>
2022-08-07 15:14:36 +0100

    Finished Product

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
 import Button from '@mui/material/Button';
 import logo from './Logo.png';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [color, setColor] = useState('red');
+  const [text, setText] = useState('Subscribe');
+
+  const handleButtonClick = () => {
+    setColor('gray');
+    setText('Subscribed');
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,12 +20,12 @@ function App() {
         <h1 className="App-title">TheIvoryCoder</h1>
         <h2 className="App-subtitle">Event Handlers in React.js</h2>
 
-        <Button variant="contained" style={{
-          backgroundColor: 'red',
+        <Button onClick={handleButtonClick} variant="contained" style={{
+          backgroundColor: color,
           fontSize: 25,
           paddingLeft: 25,
           paddingRight: 25
-        }}>Subscribe</Button>
+        }}>{text}</Button>
 
         <a className="App-link" href="https://github.com/theivorycoder/react-event-handlers">https://github.com/theivorycoder/react-event-handlers</a>
       </header>
